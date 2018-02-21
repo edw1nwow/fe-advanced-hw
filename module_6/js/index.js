c57c5c0333a62b47c6c9f0ff4ce1123ff8d66cdd
edw1nwow <best254@ukr.net>
2018-02-21 23:41:59 +0200

    asd

---
+++ module_6/js/index.js
@@ -0,0 +1,60 @@
+const keyTrainer = {
+    chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
+    checkPositiveInteger: function (inputData) {
+        const convertData = parseInt(inputData);
+        const revise = (convertData != isNaN && convertData > 0 && convertData < 33 && convertData % 1 === 0);
+        return revise;
+    },
+    setCharCount: function () {
+        let count = prompt('Count of letters', 2);
+        let condition = this.checkPositiveInteger(count);
+        while (!condition) {
+            alert('Letters must be from 1 to 33');
+            count = prompt('Count of letters', 2);
+            condition = this.checkPositiveInteger(count);
+
+        }
+        this.charCount = parseInt(count);
+    },
+    task: [],
+    createTask: function () {
+        let tempArray = [];
+        for (let i = 0; i < this.charCount; i++) {
+            const randomLetter = parseInt(Math.random() * this.chars.length);
+            tempArray.push(this.chars[randomLetter]);
+        }
+        this.task = tempArray;
+    },
+    userInput: '',
+    startTask: function () {
+        let tempStr = this.task.join('');
+        this.userInput = prompt(tempStr, );
+
+    },
+    userErrors: function () {
+        const convertToArray = this.userInput.split('');
+        let countErrors = 0;
+
+        for (let i = 0; i < this.task.length; i++) {
+            if (this.task[i] != convertToArray[i]) {
+                countErrors = countErrors + 1;
+            }
+
+        }
+        if (countErrors == 0) {
+            alert('well done');
+        } else {
+            alert('You had' + ' ' + countErrors + ' errors');
+        }
+    },
+    run: function () {
+        keyTrainer.setCharCount();
+        keyTrainer.createTask();
+        keyTrainer.startTask();
+        keyTrainer.userErrors();
+        const result = keyTrainer.userInput;
+        console.log('result', result)
+    }
+}
+
+keyTrainer.run();
