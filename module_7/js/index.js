a2695a7b75ae103fece46cfc73092cf00c3af3f8
edw1nwow <best254@ukr.net>
2018-03-16 21:18:08 +0200

    c

--- module_7/js/index.js
+++ module_7/js/index.js
@@ -1,37 +1,25 @@
-const keyboard = [['q','w','e','r','t','y','u','i','o','p','[',']','\\'],['a','s','d','f','g','h','j','k','l',';','\''],['z','x','c','v','b','n','m',',','.','/']];
- 
-console.log(keyboard);
-function createLayout(){
-    for (let i=0;i<3;i++){
-        for (let j=0;j<keyboard[i].length;j++){
-           let div = document.createElement('div'); 
-            div.innerHTML = keyboard[i][j]; 
-            
-            if (i == 0){
-            div.className = "toprow";
-            const up = document.querySelector(".top")
+const keyboard = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''], ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']];
+
+function createLayout() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < keyboard[i].length; j++) {
+            let div = document.createElement('div');
+            div.innerHTML = keyboard[i][j];
+            if (i == 0) {
+                div.className = "toprow";
+                const up = document.querySelector(".top")
                 up.appendChild(div, up.firstChild);
-            
-        }
-        else if (i == 1){
-            const up = document.querySelector(".mid")
+
+            } else if (i == 1) {
+                const up = document.querySelector(".mid")
                 up.appendChild(div, up.firstChild);
-            div.className = "middlerow";
-        }
-        else if(i == 2){
-            const up = document.querySelector(".bot")
+                div.className = "middlerow";
+            } else if (i == 2) {
+                const up = document.querySelector(".bot")
                 up.appendChild(div, up.firstChild);
-            div.className = "bottomrow";
-        } 
+                div.className = "bottomrow";
+            }
         }
-       
     }
 }
 createLayout();
-
-
-
-
-
-
- 
