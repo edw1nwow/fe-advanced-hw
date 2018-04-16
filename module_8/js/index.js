8fd7efbfc86157a33f8a4462e4d5186ec9d2b676
edw1nwow <best254@ukr.net>
2018-04-16 19:59:59 +0300

    c

--- module_8/js/index.js
+++ module_8/js/index.js
@@ -1,20 +1,41 @@
 const keyboard = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''], ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']];
 
+const notes = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'];
+
 function createLayout() {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < keyboard[i].length; j++) {
             let button = document.createElement('button');
             button.innerHTML = keyboard[i][j];
             if (i == 0) {
-                button.className = "toprow button";
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
                 const up = document.querySelector(".top")
                 up.appendChild(button, up.firstChild);
-
             } else if (i == 1) {
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
                 const up = document.querySelector(".mid")
                 up.appendChild(button, up.firstChild);
                 button.className = "middlerow button";
             } else if (i == 2) {
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
                 const up = document.querySelector(".bot")
                 up.appendChild(button, up.firstChild);
                 button.className = "bottomrow button";
@@ -23,48 +44,54 @@ function createLayout() {
     }
 }
 
-function soundOn() {
-    const playSound = note => {
-        const audio = document.querySelector(`audio[data-note=${note}]`);
-        audio.currentTime = 0;
-        audio.play();
-    };
-}
+
+const playSound = note => {
+    const audio = document.querySelector(`audio[data-note=${note}]`);
+    audio.currentTime = 0;
+    audio.play();
+};
+
 
 const checker = document.querySelector(".checkBox");
 
 function recall() {
     if (checker.checked == true) {
-        soundOn();
+        playSound;
         alert('on')
     } else {
         alert("Sound off")
     }
 }
 
-const buttons = Array.from(document.querySelectorAll(" button"));
+const buttons = Array.from(document.querySelectorAll("button"));
 const temp = document.querySelector(".output")
 
 function callback() {
+
     let letter = event.key;
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < keyboard[i].length; j++) {
             if (letter == keyboard[i][j]) {
                 temp.innerHTML += letter;
-                if (i == 0){
-                const tt = document.querySelector(".toprow");
-                tt.className = "toprow button keyboard__btn--active";
-                } else if (i == 1){
-                    const tt = document.querySelector(".middlerow");
-                tt.className = "middlerow button keyboard__btn--active";
+                if (i == 0) {
+                    const tt = document.querySelectorAll(".toprow");
+                    tt[j].className = "toprow button keyboard__btn--active";
+                } else if (i == 1) {
+                    const tt = document.querySelectorAll(".middlerow");
+                    tt[j].className = "middlerow button keyboard__btn--active";
+                } else {
+                    const tt = document.querySelectorAll(".bottomrow");
+                    tt[j].className = "bottomrow button keyboard__btn--active";
                 }
-                else {
-                    const tt = document.querySelector(".bottomrow");
-                tt.className = "bottomrow button keyboard__btn--active";
-                }                
             }
         }
     }
+    window.addEventListener("keyup", callOut);
+}
+
+function callOut() {
+    let tr = document.querySelector(".keyboard__btn--active");
+    tr.classList.remove("keyboard__btn--active");
 }
 
 
